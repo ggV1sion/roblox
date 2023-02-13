411a98e74808d7fc98e82fa52389c04f9fa838fe
IIIllIlIlIIllIIllIllllII <110292384+IIIllIlIlIIllIIllIllllII@users.noreply.github.com>
2023-02-13 05:15:27 +0100

    Update aimviewer.lua

--- games/dahood/aimviewer.lua
+++ games/dahood/aimviewer.lua
@@ -1,4 +1,4 @@
-_G.Settings = {
+_G.AimViewer_Settings = {
     LocalPlayer = true,
     Keybind = "x",
     Color = Color3.fromRGB(0, 255, 0),
@@ -22,12 +22,12 @@ local LockDetector = Instance.new("Part")
 LockDetector.Parent = Services.Workspace
 LockDetector.Anchored = true
 LockDetector.CanCollide = false
-LockDetector.Material = _G.Settings.Material
-LockDetector.Color = _G.Settings.Color
-LockDetector.Transparency = _G.Settings.Transparency
+LockDetector.Material = _G.AimViewer_Settings.Material
+LockDetector.Color = _G.AimViewer_Settings.Color
+LockDetector.Transparency = _G.AimViewer_Settings.Transparency
 
 local function isLocalPlayer(d)
-    if _G.Settings.LocalPlayer then
+    if _G.AimViewer_Settings.LocalPlayer then
         return true
     else
         if d ~= Local.Player then
@@ -41,8 +41,8 @@ end
 local function getClosestPlayerToCursor()
     local closest, shortest = nil, math.huge
     for _, player in next, Services.Players:GetPlayers() do
-        if c(player) and player.Character and player.Character:FindFirstChild("Humanoid") and
-            player.Character.Humanoid.Health ~= 0 and player.Character:FindFirstChild(_G.Settings.Part) then
+        if isLocalPlayer(player) and player.Character and player.Character:FindFirstChild("Humanoid") and
+            player.Character.Humanoid.Health ~= 0 and player.Character:FindFirstChild(_G.AimViewer_Settings.Part) then
             local pos = Services.Workspace.CurrentCamera:WorldToViewportPoint(player.Character.PrimaryPart.Position)
             local magnitude = (Vector2.new(pos.X, pos.Y) - Vector2.new(Local.Mouse.X, Local.Mouse.Y)).magnitude
             if magnitude < shortest then
@@ -57,7 +57,7 @@ end
 local player, enabled = nil, false
 
 Local.Mouse.KeyDown:Connect(function(key)
-    if key == _G.Settings.Keybind then
+    if key == _G.AimViewer_Settings.Keybind then
         if enabled then
             enabled = false
             player = nil
@@ -81,16 +81,17 @@ end)
 Services.RunService.Stepped:Connect(function()
     if player and player.Character:FindFirstChildWhichIsA("Tool") and
         player.Character:FindFirstChildWhichIsA("Tool"):FindFirstChild("GunScript") and
-        Services.Players:FindFirstChild(tostring(player)) and player.Character:FindFirstChild(_G.Settings.Part) then
-        local distance = (player.Character[_G.Settings.Part].CFrame.Position -
+        Services.Players:FindFirstChild(tostring(player)) and
+        player.Character:FindFirstChild(_G.AimViewer_Settings.Part) then
+        local distance = (player.Character[_G.AimViewer_Settings.Part].CFrame.Position -
                              player.Character.BodyEffects.MousePos.Value).Magnitude
-        local mid = (player.Character[_G.Settings.Part].CFrame.Position + player.Character.BodyEffects.MousePos.Value) /
-                        2
-        LockDetector.Size = Vector3.new(_G.Settings.Size[1], _G.Settings.Size[2], distance)
+        local mid = (player.Character[_G.AimViewer_Settings.Part].CFrame.Position +
+                        player.Character.BodyEffects.MousePos.Value) / 2
+        LockDetector.Size = Vector3.new(_G.AimViewer_Settings.Size[1], _G.AimViewer_Settings.Size[2], distance)
         LockDetector.CFrame = CFrame.lookAt(mid, player.Character.BodyEffects.MousePos.Value, Vector3.new(0, 1, 0))
-        LockDetector.Material = _G.Settings.Material
-        LockDetector.Color = _G.Settings.Color
-        LockDetector.Transparency = _G.Settings.Transparency
+        LockDetector.Material = _G.AimViewer_Settings.Material
+        LockDetector.Color = _G.AimViewer_Settings.Color
+        LockDetector.Transparency = _G.AimViewer_Settings.Transparency
     else
         LockDetector.Size = Vector3.new(0, 0, 0)
         LockDetector.CFrame = CFrame.new(0, 0, 0)
