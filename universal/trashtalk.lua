6b08b18792fb26c315a838ee3cc8828429850d52
luatruth <110292384+luatruth@users.noreply.github.com>
2023-02-13 09:38:50 +0100

    Create trashtalk.lua

---
+++ universal/trashtalk.lua
@@ -0,0 +1,22 @@
+local Players = game:GetService("Players")
+
+_G.TrashTalk_Settings = {
+    Keybind = "m",
+    Texts = {"focus", "you're terrible", "cold"}
+}
+
+local Services = {
+    Players = game:GetService("Players"),
+    ReplicatedStorage = game:GetService("ReplicatedStorage")
+}
+
+local Local = {
+    Mouse = Services.Players.LocalPlayer:GetMouse()
+}
+
+Local.Mouse.KeyDown:Connect(function(key)
+    if key == string.lower(_G.TrashTalk_Settings.Keybind) then
+        Services.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
+            _G.TrashTalk_Settings.Texts[math.random(1, #_G.TrashTalk_Settings.Texts)], "All")
+    end
+end)
